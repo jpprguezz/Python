24bf1e20bf00c8e6f662e0ef4d4c5b68a371a008
jpprguezz <josepenrod@gmail.com>
2024-08-22 09:14:33 +0100

    Se hace el ejercicio lost_word.py

--- Cadenas_de_Texto/Ejercicios/lost_word.py
+++ Cadenas_de_Texto/Ejercicios/lost_word.py
@@ -5,10 +5,12 @@
 
 def run(text: str, target_word: str, replace_word: str) -> str:
     # TU CÓDIGO AQUÍ
-    mtext = 'output'
-
+    replacing_slicing = text.find(target_word)
+    start = text[:replacing_slicing]
+    end = text[replacing_slicing + len(target_word):]  # Sumamos la longitud de la target_word para saltar exactamente sobre la palabra que estamos reemplazando, de modo que podamos capturar correctamente el resto del texto a partir de donde termina la palabra original.
+    mtext = start + replace_word + end
     return mtext
 
 
-if __name__ == '__main__':
-    run('This is a beautiful night on the Atlantic', 'beautiful', 'terrible')
+if __name__ == "__main__":
+    run("This is a beautiful night on the Atlantic", "beautiful", "terrible")
